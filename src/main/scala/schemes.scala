66fd5696d148e75cc12495cd80c91db16237a124
intsuc <intsuc@gmail.com>
2021-07-28 06:14:31 +0900

    Remove type applications

--- src/main/scala/schemes.scala
+++ src/main/scala/schemes.scala
@@ -1,7 +1,7 @@
 extension [F[*]: Functor, A](fix: Fix[F])
   def cata(algebra: Algebra[F, A]): A =
-    algebra(summon[Functor[F]](fix.unfix, _ cata algebra))
+    algebra(summon(fix.unfix, _ cata algebra))
 
 extension [F[*]: Functor, A](a: A)
   def ana(coalgebra: Coalgebra[F, A]): Fix[F] =
-    summon[Functor[F]](coalgebra(a), _ ana coalgebra).fix
+    summon(coalgebra(a), _ ana coalgebra).fix
